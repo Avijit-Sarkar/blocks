be4bfa5494b0715df38a937814ac88baa9035cdc
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-30 01:46:22 +0530

    creating the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import NavBar from "@/components/NavBar";
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,10 +17,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body
+        className="min-h-screen pt-12 bg-slate-100  dark:bg-background antialiased"
+        suppressHydrationWarning={true}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          <NavBar />
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
